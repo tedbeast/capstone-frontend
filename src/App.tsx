e459a52b04bbbb83524739740e233daeb5cc3e06
ksstringer <xqqmoi@gmail.com>
2024-03-25 14:42:08 -0400

    login team adding welcome page, nav bar, and page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { PageNotFoundPage } from "./Pages/PageNotFound";
+import { WelcomePage } from "./Pages/WelcomePage";
+import { NavigationBar } from "./Components/NavigationBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Login Team was here</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <NavigationBar></NavigationBar>
+      <Routes>
+        <Route path="home" element={<WelcomePage></WelcomePage>}></Route>
+        <Route path="/" element={<WelcomePage></WelcomePage>}></Route>
+        <Route path="*" element={<PageNotFoundPage></PageNotFoundPage>}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/NavigationBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Nav.css';
+
+export function NavigationBar() {
+    return (<>
+    <ul>
+        <li><Link to="home">Home Page</Link></li>
+    </ul>
+    </>);
+}
