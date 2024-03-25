3ad69ee0bde409399422dcceeaca68f2d3357521
ksstringer <xqqmoi@gmail.com>
2024-03-25 14:35:49 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { NavigationBar } from './Components/NavigationBar';
-import { WelcomePage } from './Pages/WelcomePage';
-import { PageNotFoundPage } from './Pages/PageNotFound';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <>
-    <BrowserRouter>
-    <NavigationBar></NavigationBar>
-      <Routes>
-        <Route path="home" element={<WelcomePage></WelcomePage>}></Route>
-        <Route path="/" element={<WelcomePage></WelcomePage>}></Route>
-        <Route path="*" element={<PageNotFoundPage></PageNotFoundPage>}></Route>
-      </Routes>
-    </BrowserRouter>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <p>Login Team was here</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
