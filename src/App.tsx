7c61316fc66af7a6987595b51e8ea00813860a8d
ksstringer <xqqmoi@gmail.com>
2024-03-27 12:21:28 -0400

    fixed router issues and naming conventions

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { PageNotFoundPage } from "./Pages/PageNotFound";
-import { WelcomePage } from "./Pages/WelcomePage";
-import { NavigationBar } from "./Components/NavigationBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { PasswordResetPage } from "./Pages/PasswordResetPage";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage'
+import { Homepage } from './pages/Homepage'
+import { NavigationBar } from './components/NavigationBar';
+import { PasswordResetPage } from './pages/PasswordResetPage';
+import { WelcomeLoggedInUser } from './pages/WelcomeLoggedInUser';
 
 function App() {
   return (
     <>
     <BrowserRouter>
     <NavigationBar></NavigationBar>
-      <Routes>
-        <Route path="home" element={<WelcomePage></WelcomePage>}></Route>
-        <Route path="/" element={<WelcomePage></WelcomePage>}></Route>
-        <Route path="*" element={<PageNotFoundPage></PageNotFoundPage>}></Route>
-        <Route path="password reset" element={<PasswordResetPage></PasswordResetPage>}></Route>
-      </Routes>
+      <Route path="" component={Homepage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/welcome" component={WelcomeLoggedInUser} />
+      <Route path="/passwordreset" component={PasswordResetPage} />
     </BrowserRouter>
     </>
   );
