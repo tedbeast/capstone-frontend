8f3f3ec31cf29a8a7f8d47d699607c529156c9c0
mrggripp <sgrippaudo90@gmail.com>
2024-03-27 09:49:33 -0400

    changes to performance review front end to fix lowercase model issues

--- src/Components/PerformanceListByManager.tsx
+++ src/Components/PerformanceListByManager.tsx
@@ -1,39 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { PerformanceReview } from "../models/PerformanceReview";
+import { PerformanceReview } from "../Models/PerformanceReview";
 import { getAllPerformanceByManagerAPI } from "../services/GoalsAPIService";
-import { Manager } from "../models/Manager";
+import { Manager } from "../Models/Manager";
 import { PerformanceReviewEntry } from "./SinglePerformanceByManager";
 
+export function PerformanceReviewList(props: { managerIdProp: number }) {
+  const [allPerformance, setAllPerformance] = useState<PerformanceReview[]>([]);
 
+  useEffect(() => {
+    getAllPerformanceByManagerAPI(props.managerIdProp)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setAllPerformance(json);
+      });
+  }, []);
 
-export function PerformanceReviewList(props: { managerIdProp: number; }) {
+  function logAllPerformance() {
+    //call the function to get all performance reviews
+    getAllPerformanceByManagerAPI(props.managerIdProp)
+      //when promise returns, define what is the response
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setAllPerformance(json);
+      });
+  }
 
-    const [allPerformance, setAllPerformance] = useState<PerformanceReview[]>([])
-
-    useEffect(() => {
-        getAllPerformanceByManagerAPI(props.managerIdProp)
-            .then(response => { return response.json() })
-            .then(json => { setAllPerformance(json); });
-    }, []);
-
-    function logAllPerformance() {
-        //call the function to get all performance reviews
-        getAllPerformanceByManagerAPI(props.managerIdProp)
-        //when promise returns, define what is the response
-        .then(response => {return response.json()})
-        .then(json => {setAllPerformance(json)})
-
-    }
-
-    return (
-        <>
-        <p>
-        <button onClick={logAllPerformance}>Show List</button> 
-        </p>
-        {allPerformance.map(performanceReview => {
-            return <PerformanceReviewEntry key={performanceReview.performanceReviewID} data={performanceReview}></PerformanceReviewEntry>
-        })}
-        </>
-    )
-
+  return (
+    <>
+      <p>
+        <button onClick={logAllPerformance}>Show List</button>
+      </p>
+      {allPerformance.map((performanceReview) => {
+        return (
+          <PerformanceReviewEntry
+            key={performanceReview.performanceReviewID}
+            data={performanceReview}
+          ></PerformanceReviewEntry>
+        );
+      })}
+    </>
+  );
 }
