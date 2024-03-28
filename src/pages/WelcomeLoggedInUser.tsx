fdef1e0e67546ccdd843310af9d7413cff7fe98f
galina101 <goncharovagalinka@gmail.com>
2024-03-28 10:31:51 -0400

    Added logout button, user role

--- src/pages/WelcomeLoggedInUser.tsx
+++ src/pages/WelcomeLoggedInUser.tsx
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
-import { Login } from "../components/Login";
+import React, {useState} from 'react';
+import {Login} from "../components/Login";
 import {useHistory} from "react-router-dom";
 
 export function WelcomeLoggedInUser() {
-    const userId = localStorage.getItem('username');
-    const userRole = localStorage.getItem('role');
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const history = useHistory();
+  const userId = localStorage.getItem('username');
+  const userRole = localStorage.getItem('role');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const history = useHistory();
 
-    const handleLogin = () => {
-        setIsLoggedIn(true);
-    };
-    const handleLogOut = () => {
-        console.log ("User logged out.");
-        localStorage.removeItem('username');
-        localStorage.removeItem('role');
-        setIsLoggedIn(false);
-        history.push('/login');
-    }
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogOut = () => {
+    console.log("User logged out.");
+    localStorage.removeItem('username');
+    localStorage.removeItem('role');
+    setIsLoggedIn(false);
+    history.push('/login');
+  }
 
-    return (
-        <>
-            {(userId) ? (
-                <>
-                    <h1>Welcome, {userRole}!</h1> {/* Display the user's role */}
-                    {/* Any other content for the welcome page */}
-                    <button onClick={handleLogOut}> Log Out</button>
-                </>
-            ) : (
-                <Login onLogin={handleLogin} />
-            )}
-        </>
-    );
+  return (
+      <>
+        {(userId) ? (
+            <>
+              <h1>Welcome, {userRole}!</h1> {/* Display the user's role */}
+              {/* Any other content for the welcome page */}
+              <button onClick={handleLogOut}> Log Out</button>
+            </>
+        ) : (
+            <Login onLogin={handleLogin}/>
+        )}
+      </>
+  );
 }
