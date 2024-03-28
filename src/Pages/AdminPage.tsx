ebabef93cc51935dc55d1fff2f0e585caa1b1762
Sblalock0806 <steven.blalock0217@gmail.com>
2024-03-28 08:23:19 -0400

    Added dropdown list to admin page

--- src/Pages/AdminPage.tsx
+++ src/Pages/AdminPage.tsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState, ReactElement } from 'react';
+import { Dropdown, DropdownButton, Modal, Button } from 'react-bootstrap'; // Assuming you're using react-bootstrap
 import UpdateAdmin from "../Components/UpdateAdmin";
 import AddAdmin from "../Components/AddAdmin";
 import AddManager from "../Components/AddManager";
 import { EmployeeList } from "../Components/EmployeeList";
 import DeleteEmployee from "../Components/DeleteEmployee";
-export function AdminPage(){
-    return (<>
-    <AddAdmin></AddAdmin>
-    <UpdateAdmin></UpdateAdmin>
-    <AddManager></AddManager>
-    <DeleteEmployee></DeleteEmployee>
-    <EmployeeList></EmployeeList>
-    </>)
+
+export function AdminPage() {
+    const [selectedComponent, setSelectedComponent] = useState<string | null>(null);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const components: { [key: string]: ReactElement } = {
+        'Add Manager': <AddManager />,
+        'Add Employee': <AddAdmin />,
+        'Update Admin': <UpdateAdmin />,
+        'Delete Employee': <DeleteEmployee />
+    };
+
+    return (
+        <>
+            <DropdownButton id="dropdown-basic-button" title={selectedComponent ? selectedComponent : "Choose an Option"}>
+                {Object.keys(components).map((key) => (
+                    <Dropdown.Item onClick={() => {setSelectedComponent(key); handleShow();}}>{key}</Dropdown.Item>
+                ))}
+            </DropdownButton>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{selectedComponent}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {selectedComponent && components[selectedComponent]}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <EmployeeList />
+        </>
+    );
 }
