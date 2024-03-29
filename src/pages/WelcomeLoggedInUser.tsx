7e94fe756871f926da9263027f89d83b6086ba06
RacGray <RachelWorswick@yahoo.com>
2024-03-28 23:00:21 -0400

    Added Enhancements to Global Nav Bar; includes working nav links as placeholders for now; includes dropdown, css styling and icons.

--- src/pages/WelcomeLoggedInUser.tsx
+++ src/pages/WelcomeLoggedInUser.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
-import {Login} from "../components/Login";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { Login } from "../components/Login";
+import { useNavigate } from "react-router-dom";
 
 export function WelcomeLoggedInUser() {
-  const userId = localStorage.getItem('username');
-  const userRole = localStorage.getItem('role');
+  const userId = localStorage.getItem("username");
+  const userRole = localStorage.getItem("role");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
 
@@ -13,23 +13,23 @@ export function WelcomeLoggedInUser() {
   };
   const handleLogOut = () => {
     console.log("User logged out.");
-    localStorage.removeItem('username');
-    localStorage.removeItem('role');
+    localStorage.removeItem("username");
+    localStorage.removeItem("role");
     setIsLoggedIn(false);
-    navigate('/login');
-  }
+    navigate("/login");
+  };
 
   return (
-      <>
-        {(userId) ? (
-            <>
-              <h1>Welcome, {userRole}!</h1> {/* Display the user's role */}
-              {/* Any other content for the welcome page */}
-              <button onClick={handleLogOut}> Log Out</button>
-            </>
-        ) : (
-            <Login onLogin={handleLogin}/>
-        )}
-      </>
+    <>
+      {userId ? (
+        <>
+          <h1>Welcome, {userRole}!</h1> {/* Display the user's role */}
+          {/* Any other content for the welcome page */}
+          <button onClick={handleLogOut}> Log Out</button>
+        </>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
+    </>
   );
 }
