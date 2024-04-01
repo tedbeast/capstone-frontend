cd701e76cfaab529773000a65ea9b5483a4a478e
yagmurozkan2 <yagmurozkan292@gmail.com>
2024-04-01 16:45:28 -0400

    Added autorefresh to listgoal

--- src/Components/AddGoal.tsx
+++ src/Components/AddGoal.tsx
@@ -2,10 +2,15 @@ import React, { SyntheticEvent, useState } from "react";
 import { Goal } from "../Models/Goal";
 import { postGoalsAPI } from "../Services/GoalsAPIService";
 
-export function AddGoal() {
+interface AddGoalInterface {
+  goalCounterFunction: Function;
+}
+
+export function AddGoal(props : AddGoalInterface) {
   const [userGoalType, setUserGoalType] = useState("");
   const [userGoalDescription, setUserGoalDescription] = useState("");
-  const [goalAPIResut, setGoalAPIResult] = useState("");
+  
+  const [goalCounter, setGoalCounter] = useState(1);
 
   function goalTypeHandler(event: SyntheticEvent) {
     let goalTypeTextBox = event.target as HTMLTextAreaElement;
@@ -29,13 +34,9 @@ export function AddGoal() {
     };
 
     postGoalsAPI(newGoal, 1)
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-      });
-    //.then(() => {setGoalAPIResult("Goal added successfully!"); setTimeout(window.location.reload.bind(window.location), 1500);})
+    .then((response) => {return response.json();})
+    .then((json) => {console.log(json);})
+    .then(() => {setGoalCounter(goalCounter+1); props.goalCounterFunction(goalCounter);});
   }
 
   return (
@@ -43,22 +44,13 @@ export function AddGoal() {
       <h1>Enter your Goals</h1>
       <div className="w3-half">
         <label>Goal Type: </label>
-        <input
-          type="text"
-          onChange={goalTypeHandler}
-          value={userGoalType}
-        ></input>
+        <input type="text" onChange={goalTypeHandler} value={userGoalType}></input>
       </div>
       <div className="w3-half">
         <label>Goal Description: </label>
-        <input
-          type="text"
-          onChange={goalDescriptionHandler}
-          value={userGoalDescription}
-        ></input>
+        <input type="text" onChange={goalDescriptionHandler} value={userGoalDescription}></input>
       </div>
       <button onClick={buttonClickHandler}>Submit</button>
-      <p>{goalAPIResut}</p>
     </>
   );
 }
