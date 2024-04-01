df181ab49c292aa2bc2e25ab5fd0913f7e394872
JenGWright <jkbgannon@gmail.com>
2024-04-01 10:55:37 -0400

    holiday list

--- src/Pages/LeavesPage.tsx
+++ src/Pages/LeavesPage.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Welcome } from "../Components/Welcome";
+import HolidayLeave from "../Components/HolidayLeave";
+
 
 export function LeavesPage() {
-    return (<>
+    return (
+        <div>
     <h1>Welcome to leaves page</h1>
-    </>)
+   <HolidayLeave />
+    </div>
+
+    );
 }
+
+export default LeavesPage;
