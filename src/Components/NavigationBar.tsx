1b33a03ec8e3bc077c0594467ad4ad2968b367fb
RacGray <RachelWorswick@yahoo.com>
2024-04-02 17:54:50 -0400

    Added Bus Logo to Nav Bar; changed NavBar DropMenu  name from 'Menu' to 'My Account'  Changes to be committed: 	modified:   package-lock.json 	modified:   src/Components/Nav.css 	modified:   src/Components/NavigationBar.tsx 	new file:   src/Images/HR (7).png

--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -4,11 +4,12 @@ import "./Nav.css";
 import "../Components/Nav.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faSquareCaretDown } from "@fortawesome/free-solid-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faDoorClosed } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faKey } from "@fortawesome/free-solid-svg-icons";
 import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 import { faPersonThroughWindow } from "@fortawesome/free-solid-svg-icons";
+import logo from "../Images/HR (7).png";
 
 export function NavigationBar() {
   return (
@@ -16,35 +17,46 @@ export function NavigationBar() {
       <ul>
         <li className="dropdown">
           {" "}
-          <FontAwesomeIcon icon={faBars} className="icon"/> Menu
+          <FontAwesomeIcon icon={faUser} className="icon" /> My Account{" "}
           <ul className="dropdown-content">
             <li>
               {" "}
               <Link to="login">
                 {" "}
-                <FontAwesomeIcon icon={faUser} className="icon"/> Login
+                <FontAwesomeIcon
+                  icon={faDoorOpen}
+                  className="icon"
+                /> Login{" "}
               </Link>
             </li>
-            <li>
-              <Link to="WelcomeLoggedInUser">
-                <FontAwesomeIcon icon={faDoorOpen} className="icon"/> Welcome
-              </Link>
-            </li>
+
+            {/* <li><Link to="WelcomeLoggedInUser"><FontAwesomeIcon icon={faUser} className="icon" /> Welcome</Link></li> */}
+
             <li>
               <Link to="leave">
                 <FontAwesomeIcon
-                    icon={faPersonThroughWindow}
-                    className="icon"
+                  icon={faPersonThroughWindow}
+                  className="icon"
                 />{" "}
-                Leave Requests
-              </Link>
+                Leave Requests{" "}
+              </Link>{" "}
             </li>
+
             <li>
               <Link to="passwordreset">
-                <FontAwesomeIcon icon={faKey} className="icon"/> Password Reset
+                <FontAwesomeIcon icon={faKey} className="icon" /> Password Reset
+              </Link>
+            </li>
+
+            <li>
+              <Link to="logout">
+                <FontAwesomeIcon icon={faDoorClosed} className="icon" /> Logout
               </Link>
             </li>
           </ul>
+        </li>
+        <li className="logo">
+          <img src={logo} alt="Logo" className="logo" />
         </li>
         <li>
           <Link to="leave">Leave Requests</Link>
@@ -61,12 +73,13 @@ export function NavigationBar() {
         <li>
           <Link to="Reporting">Reporting Page</Link>
         </li>
-        <li>
+
+        {/* <li>
           <Link to="login">Login Page</Link>
         </li>
         <li>
           <Link to="passwordreset">Password Reset</Link>
-        </li>
+        </li> */}
       </ul>
     </>
   );
