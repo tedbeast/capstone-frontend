9e53ab9fbe11da8ea0806f3e328bb34a0ff1d089
thbianco-rev <thomasnbianco@gmail.com>
2024-04-03 11:57:56 -0400

    Password reset changes by Kelly and Thomas

--- src/Components/PasswordReset.tsx
+++ src/Components/PasswordReset.tsx
@@ -1,34 +1,25 @@
-import React, {SyntheticEvent, useState} from "react";
-import {Employee} from "../Models/Employee";
-import {getEmployeeById, putPassword} from "../Services/LoginAPIService";
-import {Roles} from "../Models/Roles";
-import {Manager} from "../Models/Manager";
-import {PerformanceReview} from "../Models/PerformanceReview";
-import {Leaves} from "../Models/Leaves";
-import {ResetPasswordEmployee} from "../Models/ResetPasswordEmployee";
+import React, { SyntheticEvent, useState } from "react";
+import { Employee } from "../Models/Employee";
+import { getEmployeeById, putPassword } from "../Services/LoginAPIService";
 
-//interface propsInterface {
-//    data:Employee
-//}
+export function ResetPassword(props: { data: { employeeId: any } }) {
+  const [showForm, setShowForm] = useState(true);
+  const [showSuccess, setShowSuccess] = useState(false);
 
-//type PublicProps = propsInterface;
+  const [employeeIdInput, setEmployeeIdInput] = useState<number | undefined>();
+  const [passwordInput, setPasswordInput] = useState<string>("");
+  const [confirmPasswordInput, setConfirmPasswordInput] = useState<string>("");
 
-export function ResetPassword(props: { data: { employeeId: any; }; }) {
-  const [employeeIdInput, setEmployeeIdInput] = useState<number | undefined>()
-  const [passwordInput, setPasswordInput] = useState<string>("")
-  const [confirmPasswordInput, setConfirmPasswordInput] = useState<string>("")
-  const [alertMessage, setAlertMessage] = useState<string>("")
+  const [alertMessage, setAlertMessage] = useState<string>("");
 
   function employeeIdInputHandler(event: SyntheticEvent) {
     let idBox = event.target as HTMLInputElement;
     setEmployeeIdInput(parseInt(idBox.value));
   }
-
   function passwordInputHandler(event: SyntheticEvent) {
     let passwordBox = event.target as HTMLInputElement;
     setPasswordInput(passwordBox.value);
   }
-
   function confirmPasswordInputHandler(event: SyntheticEvent) {
     let confirmPasswordBox = event.target as HTMLInputElement;
     setConfirmPasswordInput(confirmPasswordBox.value);
@@ -37,88 +28,99 @@ export function ResetPassword(props: { data: { employeeId: any; }; }) {
   const resetSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setAlertMessage("");
-
-    let pwd = passwordInput
-
+    let id = employeeIdInput;
+    let pwd = passwordInput;
+    let response = await getEmployeeById(id);
+    let validity = response.status;
     if (!employeeIdInput) {
-      return setAlertMessage("Employee ID cannot be blank")
+      return setAlertMessage("Employee ID cannot be blank.");
     }
-
+    if (validity === 404) {
+      return setAlertMessage("Employee ID does not exist.");
+    }
     if (!passwordInput || !confirmPasswordInput) {
-      return setAlertMessage("Password cannot be blank")
+      return setAlertMessage("Password cannot be blank.");
     }
     if (passwordInput !== confirmPasswordInput) {
-      return setAlertMessage("Passwords must match");
+      return setAlertMessage("Passwords must match.");
     }
-    let id = employeeIdInput;
-    let validEmployee = await getEmployeeById(id)
-    let validity = validEmployee.status;
-
-    if (validity == 404) {
-      return setAlertMessage("Employee ID does not exist")
+    let data = await response.json();
+    let employee: Employee = data;
+    employee.password = pwd;
+    console.log(employee);
+    let pwdApiCall = await putPassword(employee);
+    let pwdSuccess = pwdApiCall.status;
+    if (pwdSuccess === 400) {
+      return setAlertMessage(
+        "New password cannot be the same as the current password."
+      );
     }
+    if (pwdSuccess === 200) {
+      setShowForm(false);
+      setShowSuccess(true);
+    }
+  };
 
-    // let employee: Employee = {
-    //     employeeID: id,
-    //     password: pwd
-    //  }
-    // let employee: ResetPasswordEmployee = {
-    //   employeeID: id,
-    //   password: pwd
-    // }
-    // await putPassword(employee); //Requires REsetPasswordEmployee
-  }
-
-    return (
-        <div>
+  return (
+    <div>
+      {showForm && (
+        <form onSubmit={resetSubmit}>
           <h3>Please enter the below information to reset your password.</h3>
-          {alertMessage !== "" &&
-              <div style={{color: "red", paddingBottom: 10}}>
-                {alertMessage}
-              </div>}
-          <form onSubmit={resetSubmit}>
-            <div>
-              <label>
-                EmployeeID
-                <div>
-                  <input
-                      name="employeeid"
-                      type="employeeid"
-                      onChange={employeeIdInputHandler}
-                  />
-                </div>
-              </label>
+          {alertMessage !== "" && (
+            <div style={{ color: "red", paddingBottom: 10 }}>
+              {alertMessage}
             </div>
-            <div>
-              <label>
-                Password
-                <div>
-                  <input
-                      name="password"
-                      type="password"
-                      onChange={passwordInputHandler}
-                  />
-                </div>
-              </label>
-            </div>
-            <div>
-              <label>
-                Confirm Password
-                <div>
-                  <input
-                      name="confirmpassword"
-                      type="password"
-                      onChange={confirmPasswordInputHandler}
-                  />
-                </div>
-              </label>
-            </div>
-            <button type="submit">Reset Password</button>
-          </form>
-        </div>
-    )
-
-    //value={employeeIdInput}
-    //value={passwordInput}
-    //value={confirmPasswordInput}
-  }
+          )}
+          <div>
+            <label>
+              EmployeeID
+              <div>
+                <input
+                  name="employeeid"
+                  type="employeeid"
+                  onChange={employeeIdInputHandler}
+                />
+              </div>
+            </label>
+          </div>
+          <div>
+            <label>
+              Password
+              <div>
+                <input
+                  name="password"
+                  type="password"
+                  onChange={passwordInputHandler}
+                />
+              </div>
+            </label>
+          </div>
+          <div>
+            <label>
+              Confirm Password
+              <div>
+                <input
+                  name="confirmpassword"
+                  type="password"
+                  onChange={confirmPasswordInputHandler}
+                />
+              </div>
+            </label>
+          </div>
+          <button type="submit">Reset Password</button>
+        </form>
+      )}
+      {showSuccess && (
+        <>
+          <div style={{ fontSize: 20, paddingTop: 10, paddingBottom: 5 }}>
+            Password successfully updated!
+          </div>
+          <div style={{ paddingBottom: 10 }}>
+            Please return to the Login Page and log in.
+          </div>
+          <a href="/login">Return to login</a>
+        </>
+      )}
+    </div>
+  );
+}
