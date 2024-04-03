ff7ef277936e13d062559e8716249180175b4056
RacGray <RachelWorswick@yahoo.com>
2024-04-02 20:47:36 -0400

    Updated the nav bar code and menu options. Updated  css for nav bar and whole app.css file.

--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -10,11 +10,88 @@ import { faKey } from "@fortawesome/free-solid-svg-icons";
 import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 import { faPersonThroughWindow } from "@fortawesome/free-solid-svg-icons";
 import logo from "../Images/HR (7).png";
+import { useState } from "react";
 
 export function NavigationBar() {
+  const [useState] = React.useState(false);
   return (
     <>
-      <ul>
+      <div id="header">
+        <Link id="login-link" to={useState ? "/WelcomeLoggedInUser" : "/Login"}>
+          <img src={logo} alt="Logo" id="header-logo" />
+        </Link>
+
+        <div id="nav-container">
+          <ul id="nav">
+            <li className="dropdown">
+              {" "}
+              <FontAwesomeIcon icon={faUser} className="icon" /> My Account{" "}
+              <ul className="dropdown-content">
+                <li>
+                  {" "}
+                  <Link to="login">
+                    {" "}
+                    <FontAwesomeIcon
+                      icon={faDoorOpen}
+                      className="icon"
+                    /> Login{" "}
+                  </Link>
+                </li>
+
+                {/* <li>
+                  <Link to="leave">
+                    <FontAwesomeIcon
+                      icon={faPersonThroughWindow}
+                      className="icon"
+                    />{" "}
+                    Leave Requests{" "}
+                  </Link>{" "}
+                </li> */}
+
+                <li>
+                  <Link to="passwordreset">
+                    <FontAwesomeIcon icon={faKey} className="icon" /> Password
+                    Reset
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="logout">
+                    <FontAwesomeIcon icon={faDoorClosed} className="icon" />{" "}
+                    Logout
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="dropdown">
+              {" "}
+              Leave{" "}
+              <ul className="dropdown-content">
+                <li>
+                  <Link to="leave">
+                    <FontAwesomeIcon
+                      icon={faPersonThroughWindow}
+                      className="icon"
+                    />{" "}
+                    Leave Requests{" "}
+                  </Link>{" "}
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="Goals">Goals</Link>
+            </li>
+            <li>
+              <Link to="Admin">Administrative</Link>
+            </li>
+            <li>
+              <Link to="Reporting">Reporting</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* <ul id="nav">
         <li className="dropdown">
           {" "}
           <FontAwesomeIcon icon={faUser} className="icon" /> My Account{" "}
@@ -30,7 +107,7 @@ export function NavigationBar() {
               </Link>
             </li>
 
-            {/* <li><Link to="WelcomeLoggedInUser"><FontAwesomeIcon icon={faUser} className="icon" /> Welcome</Link></li> */}
+            {// <li><Link to="WelcomeLoggedInUser"><FontAwesomeIcon icon={faUser} className="icon" /> Welcome</Link></li>}
 
             <li>
               <Link to="leave">
@@ -74,13 +151,13 @@ export function NavigationBar() {
           <Link to="Reporting">Reporting Page</Link>
         </li>
 
-        {/* <li>
-          <Link to="login">Login Page</Link>
-        </li>
-        <li>
-          <Link to="passwordreset">Password Reset</Link>
-        </li> */}
-      </ul>
+        //{<li>
+          //<Link to="login">Login Page</Link>
+        //</li>
+        //<li>
+          //<Link to="passwordreset">Password Reset</Link>
+        //</li> }
+      </ul> */}
     </>
   );
 }
